9c021cf3b46829f1a23c997f39377b244943c715
조상호 <hyundai_sangho@naver.com>
2022-10-13 23:53:08 +0900

    functions 학습

---
+++ 08_functions.php
@@ -0,0 +1,40 @@
+<?php
+
+// Function which prints "Hello I am chosangho"
+
+// function hello($name)
+// {
+//   echo 'Hello I am ' . $name . '<br />';
+// }
+
+// hello('조상호');
+// hello('이민재');
+// hello('지성현');
+
+// Create sum of two functions
+// function sum($a, $b)
+// {
+//   return $a + $b;
+// }
+
+// $sum = sum(4, 5);
+// echo $sum . '<br>';
+
+// Create function to sum all numbers using ...$nums
+function sum(...$numbers)
+{
+  // echo '<pre>';
+  // var_dump($numbers);
+  // echo '</pre>';
+
+  // $sum = 0;
+  // foreach ($numbers as $num) $sum += $num;
+  // return $sum;
+
+  // return array_reduce($numbers, fn ($carry, $n) => $carry + $n);
+}
+
+// $sum = sum(1, 2, 3, 4, 5);
+// echo $sum . '<br>';
+
+// Arrow functions
