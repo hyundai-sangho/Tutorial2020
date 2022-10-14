10b103ed8e424d8732b205f2ace62dd1d25ec387
조상호 <hyundai_sangho@naver.com>
2022-10-14 21:08:23 +0900

    file system 학습

---
+++ 12_fs/index.php
@@ -0,0 +1,36 @@
+<?php
+
+// Magic constants
+echo __DIR__ . '<br>';
+echo __FILE__ . '<br>';
+
+// Create directory
+// mkdir('test');
+
+// Rename directory
+// rename('test', 'my_folder');
+
+// Delete directory
+// rmdir('my_folder');
+
+// Read files and folders inside directory
+// $files = scandir('./');
+// echo '<pre>';
+// var_dump($files);
+// echo '</pre>';
+
+// file_get_contents, file_put_contents
+// echo file_get_contents('lorem.txt');
+// file_put_contents('test.txt', 'Hello World');
+
+// $content = file_get_contents('lorem.txt');
+// file_put_contents('lorem.txt', 'My Text' . PHP_EOL . $content);
+
+// https://www.php.net/manual/en/book.filesystem.php
+// file_exists
+// is_dir
+// filemtime
+// filesize
+// disk_free_space
+// file
+https://vscode.dev/github/hyundai-sangho/Tutorial2020/blob/84518a9c2aedd43f421819f31f0dafbe2c4b251b
